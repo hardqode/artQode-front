aee81555eaec5980a6410f0c86d3439e15831667
kuroedov_maxim <Ni7pfpNh>
2018-08-17 20:13:26 +0300

    my account page added

--- js/passwordToggle.js
+++ js/passwordToggle.js
@@ -4,7 +4,9 @@ $('[btn-role="show-password"]').on('click', function () {
 
     if ($attr === 'text') {
         $input.attr('type', 'password');
+        $(this).find('img').attr('src','./assets/svg/show-password.svg')
     } else {
         $input.attr('type', 'text');
+        $(this).find('img').attr('src','./assets/svg/hide-password.svg')
     }
 })
